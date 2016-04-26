b5dc3d0e9627d0208f186285a8a5a4f9841a85ef
lycenok <lysyonok@inbox.ru>
2016-04-26 12:53:35 +0300

    test(File): add testDirectory SQL-parameter

--- Module/File/DB/Test/run.sql
+++ Module/File/DB/Test/run.sql
@@ -4,12 +4,14 @@
 -- файлов размером в 1 MB.
 --
 -- Используемые макромеременные:
+-- testDirectory              - директория для тестирования
 -- loggingLevelCode           - уровень логирования ( по-умолчанию DEBUG)
 -- httpInternetFileTest       - флаг тестирования выполнения операций по HTTP
 --                              c файлами в Интернет ( 1 да, 0 нет ( по
 --                              умолчанию))
 --
 
+@oms-default testDirectory ""
 @oms-default loggingLevelCode DEBUG
 @oms-default httpInternetFileTest 0
 
@@ -20,6 +22,13 @@ exec lg_logger_t.getRootLogger().setLevel( '&loggingLevelCode');
 set feedback on
 
 
+begin
+  if '&testDirectory' is not null then
+    pkg_Common.outputMessage( 'setting test directory');
+    pkg_FileTest.setTestDirectory( '&testDirectory');
+  end if;
+end;
+/
 
 @oms-run Test/AutoTest/unit.sql
 @oms-run Test/AutoTest/fs-operation.sql
