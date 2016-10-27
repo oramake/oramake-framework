44242ccd9a0ea799aa7850bcc46f2f3e36e1a776
mavalog <mavalog@mail.ru>
2016-10-27 16:55:47 +0300

    fix(Mail): fix bug with ignoring extra installation parameters in case set SQL_DEFINE

--- Module/Mail/DB/Install/Config/define.mk
+++ Module/Mail/DB/Install/Config/define.mk
@@ -1,3 +1,3 @@
-SQL_DEFINE += ,STOP_JOB=$(STOP_JOB)
-SQL_DEFINE += ,maxBatchWait=$(BATCH_WAIT)
-SQL_DEFINE += ,forcedJobQueueProcesses=$(JOB_QUEUE_PROCESSES)
+override SQL_DEFINE += ,STOP_JOB=$(STOP_JOB)
+override SQL_DEFINE += ,maxBatchWait=$(BATCH_WAIT)
+override SQL_DEFINE += ,forcedJobQueueProcesses=$(JOB_QUEUE_PROCESSES)
