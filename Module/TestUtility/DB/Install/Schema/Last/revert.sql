ca5b981d7a330f7f9e684acd72f6fd9955b20647
mavalog <mavalog@mail.ru>
2017-01-11 15:54:53 +0300

    chore(TestUtility): add uninstall script

---
+++ Module/TestUtility/DB/Install/Schema/Last/revert.sql
@@ -0,0 +1,10 @@
+-- script: Install/Schema/Last/revert.sql
+-- ќтмен€ет установку модул€, удал€€ созданные объекты схемы.
+
+
+-- ѕакеты
+
+drop package pkg_TestUtility
+/
+drop package pkg_Tests
+/
