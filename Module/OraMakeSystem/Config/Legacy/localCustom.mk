b159a6e3138fad1f13e5fd71f6df550a65084d2e
lycenok <lysyonok@inbox.ru>
2017-06-22 09:46:51 +0300

    docs(OraMakeSystem): clarify deprecated files documentation

--- Module/OraMakeSystem/Config/Legacy/localCustom.mk
+++ Module/OraMakeSystem/Config/Legacy/localCustom.mk
@@ -1,6 +1,6 @@
 # makefile: localCustom.mk
-
 # Устаревший файл. Оставлен для обратной совместимости со старыми версиями
-# локальных файлов OraMakeSystem в модулях
+# локальных файлов OraMakeSystem в модулях. Содержит ссылку на
+# /usr/local/etc/oms/database.mk. Копируется в /usr/local/share/oms.
 
 include /usr/local/etc/oms/database.mk
