0775196e48891bb7f11e1da2bb2991b3a5147127
mavalog <mavalog@mail.ru>
2018-02-07 15:07:14 +0300

    fix(FormatData): add granting rights to views

--- Module/FormatData/DB/Install/Grant/Last/run.sql
+++ Module/FormatData/DB/Install/Grant/Last/run.sql
@@ -27,6 +27,30 @@ for
   pkg_FormatData
 /
 
+grant select on v_fd_first_name_alias to &toUserName
+/
+create or replace synonym
+  &toUserName..v_fd_first_name_alias
+for
+  v_fd_first_name_alias
+/
+
+grant select on v_fd_middle_name_alias to &toUserName
+/
+create or replace synonym
+  &toUserName..v_fd_middle_name_alias
+for
+  v_fd_middle_name_alias
+/
+
+grant select on v_fd_no_value_alias to &toUserName
+/
+create or replace synonym
+  &toUserName..v_fd_no_value_alias
+for
+  v_fd_no_value_alias
+/
+
 
 
 undefine toUserName
