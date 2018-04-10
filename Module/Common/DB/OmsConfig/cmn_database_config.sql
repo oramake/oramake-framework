7242c1799db9cf929bc95b6e958a8388c320951a
mavalog <mavalog@mail.ru>
2018-04-10 09:03:10 +0100

    feat(Common): add ability to configure main instance name (useful for standby database)

--- Module/Common/DB/OmsConfig/cmn_database_config.sql
+++ Module/Common/DB/OmsConfig/cmn_database_config.sql
@@ -1,5 +1,8 @@
 declare
 
+  -- Имя экземпляра текущей БД (без учета main_instance_name)
+  instanceName cmn_database_config.instance_name%type;
+
   -- Настроечные данные для экземпляров БД
   cursor curData is
 select
@@ -10,6 +13,7 @@ from
     'ProdDB' as instance_name
     , 1 as is_production
     , null as ip_address_production
+    , null as main_instance_name
     , 1 as test_notify_flag
     , null as sender_domain
     , null as smtp_server
@@ -18,7 +22,7 @@ from
   from dual
   ) a
 where
-  lower( a.instance_name) = lower( pkg_Common.getInstanceName())
+  lower( a.instance_name) = lower( instanceName)
   ;
 
   rec curData%rowtype;
@@ -30,6 +34,7 @@ where
   nMerged integer := 0;
 
 begin
+  instanceName := pkg_Common.getInstanceName( ignoreMainInstanceNameFlag => 1);
   open curData;
   fetch curData into rec;
   if curData%NOTFOUND then
@@ -42,7 +47,7 @@ begin
     delete from
       cmn_database_config t
     where
-      lower( t.instance_name) <> lower( pkg_Common.GetInstanceName)
+      lower( t.instance_name) <> lower( instanceName)
       and default_flag = 0
     ;
     nDeleted := nDeleted + SQL%ROWCOUNT;
@@ -55,47 +60,51 @@ begin
     select
       'ProdDefaultDBConfig' as instance_name
       , 1 as is_production
+      , null as ip_address_production
+      , null as main_instance_name
       , 0 as test_notify_flag
       , null as sender_domain
       , null as smtp_server
       , null as notify_email
-      , null as ip_address_production
       , 1 as default_flag
     from dual
     union all
     select
       'TestDefaultDBConfig' as instance_name
       , 0 as is_production
+      , null as ip_address_production
+      , null as main_instance_name
       , 0 as test_notify_flag
       , null as sender_domain
       , null as smtp_server
       , null as notify_email
-      , null as ip_address_production
       , 1 as default_flag
     from dual
     union all
     select
-      lower( rec.instance_name) as instance_name
+      rec.instance_name as instance_name
       , rec.is_production as is_production
+      , rec.ip_address_production as ip_address_production
+      , rec.main_instance_name as main_instance_name
       , rec.test_notify_flag as test_notify_flag
       , rec.sender_domain as sender_domain
       , rec.smtp_server as smtp_server
       , rec.notify_email as notify_email
-      , rec.ip_address_production as ip_address_production
       , rec.default_flag as default_flag
     from
       dual
     where
-      lower( rec.instance_name) = lower( pkg_Common.getInstanceName())
+      lower( rec.instance_name) = lower( instanceName)
     minus
     select
-      lower( t.instance_name)
+      t.instance_name
       , t.is_production
+      , t.ip_address_production
+      , t.main_instance_name
       , t.test_notify_flag
       , t.sender_domain
       , t.smtp_server
       , t.notify_email
-      , t.ip_address_production
       , t.default_flag
     from
       cmn_database_config t
@@ -107,35 +116,41 @@ begin
   (
     instance_name
     , is_production
+    , ip_address_production
+    , main_instance_name
     , test_notify_flag
     , sender_domain
     , smtp_server
     , notify_email
-    , ip_address_production
     , default_flag
   )
   values
   (
     s.instance_name
     , s.is_production
+    , s.ip_address_production
+    , s.main_instance_name
     , s.test_notify_flag
     , s.sender_domain
     , s.smtp_server
     , s.notify_email
-    , s.ip_address_production
     , s.default_flag
   )
   when matched then update set
     d.is_production                 = s.is_production
+    , d.ip_address_production       = s.ip_address_production
+    , d.main_instance_name          = s.main_instance_name
     , d.test_notify_flag            = s.test_notify_flag
     , d.sender_domain               = s.sender_domain
     , d.smtp_server                 = s.smtp_server
     , d.notify_email                = s.notify_email
-    , d.ip_address_production       = s.ip_address_production
     , d.default_flag                = s.default_flag
   ;
   nMerged := nMerged + SQL%ROWCOUNT;
-  if nMerged = 0 then
+
+  -- Проверяем и при необходимости уточняем регистр instance_name (т.к. не
+  -- могли это сделать в merge)
+  if rec.instance_name is not null and nMerged <> 0 then
     update
       cmn_database_config t
     set
@@ -145,7 +160,6 @@ begin
       and t.instance_name <> rec.instance_name
       and default_flag = 0
     ;
-    nMerged := nMerged + SQL%ROWCOUNT;
   end if;
   close curData;
   commit;
