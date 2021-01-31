20d565316c6ebf622567037041eb822cd8728511
mavalog <mavalog@mail.ru>
2021-01-31 17:49:27 -0500

    chore(Mail): refine unique index on ml_message to decrease length of index key
    Refine index to avoid error:
    
    ORA-01450: maximum key length (6398) exceeded
    
    in Oracle 19.8 with UTF-8 charset

---
+++ Module/Mail/DB/Install/Config/2.7.0/define.mk
@@ -0,0 +1,8 @@
+LOAD_FILE_MASK = -
+
+
+override SKIP_FILE_MASK += \
+  Install/Batch/* \
+  Install/Data/* \
+
+
---
+++ Module/Mail/DB/Install/Schema/2.7.0/ml_message.sql
@@ -0,0 +1,17 @@
+drop index ml_message_ux
+/
+
+create unique index
+  ml_message_ux
+on
+  ml_message (
+    sender_address
+    , recipient_address
+    , send_date
+    , message_uid
+    , case when incoming_flag = 0 or parent_message_id is not null then
+        message_id
+      end
+  )
+tablespace &indexTablespace
+/
