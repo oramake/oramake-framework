467366d985127274c2b8ec49d450e42161c8e505
lycenok <lysyonok@inbox.ru>
2021-02-16 14:57:04 +0000

    fix(OraMakeSystem): use $ operator instead of bash in init.mk

--- Module/OraMakeSystem/Data/NewModule/DB/OmsModule/init.mk
+++ Module/OraMakeSystem/Data/NewModule/DB/OmsModule/init.mk
@@ -49,7 +49,7 @@ tab := $(empty)	$(empty)
 # build var: OMS_INSTALL_PREFIX
 # Общий префикс для инсталляционных каталогов.
 ifeq ($(OMS_INSTALL_PREFIX),)
-  binDir := $(dir $(shell bash -c "type -p oms"))
+  binDir := $(dir $(shell $$(type -p oms)))
   override OMS_INSTALL_PREFIX := $(if $(filter %/bin/,$(binDir)) \
     ,$(patsubst %/bin/,%,$(binDir)),/usr/local)
   undefine binDir
